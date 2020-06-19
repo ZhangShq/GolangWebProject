354acce49764d048d755d70951b9c3f35cb4733a
zhangshq <zhangsking@outllok.com>
2020-06-19 11:31:44 +0800

    change line

--- web_main.go
+++ web_main.go
@@ -4,7 +4,7 @@
  * @Author: zhangshq@corp.21cn.com
  * @Date: 2020-02-10 23:58:20
  * @LastEditors: zhangshq@corp.21cn.com
- * @LastEditTime: 2020-06-19 11:03:00
+ * @LastEditTime: 2020-06-19 11:30:44
  */ 
 package main
 
@@ -12,9 +12,8 @@ import (
 	"github.com/kataras/iris"
 )
 
-func main()  {
+func main(){
 	app := iris.New()
     app.Get("/", func(ctx iris.Context){})
-    app.Run(iris.Addr(":8080"))
+	app.Run(iris.Addr(":8080"))
 }
-
