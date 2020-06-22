985fd18b5e14d7bfb321ad998a7acbd25174b4f1
zhangshq <zhangsking@outllok.com>
2020-06-22 15:31:29 +0800

    add Test

--- web_main.go
+++ web_main.go
@@ -4,16 +4,26 @@
  * @Author: zhangshq@corp.21cn.com
  * @Date: 2020-02-10 23:58:20
  * @LastEditors: zhangshq@corp.21cn.com
- * @LastEditTime: 2020-06-19 11:30:44
+ * @LastEditTime: 2020-06-22 15:29:29
  */ 
 package main
 
 import (
+	"fmt"
 	"github.com/kataras/iris"
 )
 
+func TestHelloWorld(ctx iris.Context) int {
+	fmt.Println("Tst")
+	ctx.WriteString("hello world")
+	fmt.Println("Tes")
+	return 0;
+}
 func main(){
 	app := iris.New()
-    app.Get("/", func(ctx iris.Context){})
+    app.Get("/", func(ctx iris.Context){
+		fmt.Println("new connect")
+		go TestHelloWorld(ctx)
+	})
 	app.Run(iris.Addr(":8080"))
 }
